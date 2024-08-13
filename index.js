0096d72174dcb80434eccf4f815cda363d378955
Etai Hatuel <123596508+Etai89@users.noreply.github.com>
2024-08-13 10:35:00 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 //console.log(`hello`);
 //window.alert(`alert for you!`);
-document.getElementById("gooj").textContent = `bonjour!`;
+
+var et_12 = "";
+
+document.getElementById("gooj").textContent = et_12;
+
+const get_text = document.getElementById(`user`);
+const btn1 = document.getElementById(`btn`);
+const out1 = document.getElementById(`output1`)
+
+function fun(){
+    out1.innerHTML = get_text;
+}
+btn1.addEventListener(`click`, fun);
